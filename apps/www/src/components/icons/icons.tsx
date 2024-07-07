5d00f6fa34db5c76dd1047e853a9a0bf984e1e27
anascen <webovein@gmail.com>
2024-07-07 22:04:25 +0530

    refactor: adding new logo

--- apps/www/src/components/icons/icons.tsx
+++ apps/www/src/components/icons/icons.tsx
@@ -12,6 +12,6 @@ import Logo from "@/assets/logoJsxComponent/logo";
 import { Command } from "lucide-react";
 
 export const Icons = {
-    logo: <Command className="text-foreground"></Command>,
+    logo: <Logo className="text-foreground"></Logo>,
     google: <GoogleIcon></GoogleIcon>,
 };
