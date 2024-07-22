f0cbcfe016e415c999761710b1b8b7535fc67054
Varshil J Patel <123259416+varshiljpatel@users.noreply.github.com>
2024-07-22 15:03:06 +0530

    Update button.tsx

--- apps/www/src/components/ui/button.tsx
+++ apps/www/src/components/ui/button.tsx
@@ -38,10 +38,10 @@ const buttonVariants = cva(
                 link: "text-primary underline-offset-4 hover:underline",
             },
             size: {
-                default: "h-10 px-4",
-                sm: "h-9 rounded-md px-3",
-                lg: "h-11 rounded-md px-8",
-                icon: "h-10 w-10",
+                default: "h-12 px-4",
+                sm: "h-10 px-3",
+                lg: "h-14 px-8",
+                icon: "h-12 w-12",
             },
         },
         defaultVariants: {
